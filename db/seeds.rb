5a4dcdc908e7365673b5faa54575d42db48af460
Ned-el-ch <nedelchev.niki@gmail.com>
2020-01-10 16:13:15 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# categories = [
+
+# 	{
+# 		type: "Engineering"
+# 		sub_categories: ["Chemical", "Biochemical", "Mechanical", "architecture", "Electrical", "Environmental", "Civil"]
+# 	},
+# 	{
+# 		type: "Science"
+# 		sub_categories: ["Biology", "Physics", "Chemistry", "Astronomy", "Space", "Maths", "Computer Science", "Data Science", "Statistics", "Medicine", "Dentistry", "Botany", "Zoology", "Meteorology", "Oceanography", "Ecology", "Geology"]
+# 	},
+# 	{
+# 		type: "Politics"
+# 		sub_categories: ["World", "Europe", "Asia", "Africa", "North America", "South America","Australia",  "Western", "Eastern", "Middle East"]
+# 	},
+# 	{
+# 		type: "Game Development"
+# 		sub_categories: ["Game Design", "Unity 3D", "Clickteam Fusion 2.5", "Godot", "Indie", "AAA"]
+# 	},
+# 	{
+# 		type: "Programming"
+# 		sub_categories: ["JavaScript", "Java", "React", "Ruby on Rails", "APIs", "OOP", "TDD", "BDD", "Netcode", ".NET", "C#", "Redux", "Python", "SQL", "PostreSQL", "Databases", "Frontend", "Backend"]
+# 	},
+
+# ]
+
+# categories.each do |category|
+
+# 	Category.create(title: category.type)
+# 	puts category.type
+
+# 	category.sub_categories.each do |sub_category|
+
+# 		Category.create(title: sub_category)
+# 		puts sub_category
+
+# 	end
+
+# end
+
+categories = ["Science", "Engineering", "Programming", "Art", "Lifestyle", "Politics", "Game Development"]
+
+categories.each do |title|
+
+	Category.create(title: title)
+
+end
