584d461aa3669bada88bc74ab72a694dfdfcd27f
Ned-el-ch <nedelchev.niki@gmail.com>
2020-01-15 11:19:52 +0000

    added catch to see if api request to categories is valid id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,25 +1,29 @@
 class CategoriesController < ApplicationController
 	skip_before_action :authorized, only: [:posts]
 	def posts
-		byebug
 		category = Category.all.find(params[:id])
-		posts = category.posts
-		render json: posts.to_json(include: {
-			user: {
-				only: [:id, :name, :username, :bio]
-			},
-			categories: {
-				only: [:id, :title]
-			},
-			comments: {
-				only: [:id, :content, :user_id]
-			},
-			post_likes: {
-				only: [:id, :user_id]
-			},
-			post_favorites: {
-				only: [:id, :user_id]
-			}
-		})
+
+		if category.valid?
+			posts = category.posts
+			render json: posts.to_json(include: {
+				user: {
+					only: [:id, :name, :username, :bio]
+				},
+				categories: {
+					only: [:id, :title]
+				},
+				comments: {
+					only: [:id, :content, :user_id]
+				},
+				post_likes: {
+					only: [:id, :user_id]
+				},
+				post_favorites: {
+					only: [:id, :user_id]
+				}
+			})
+		else
+			render json: { error: 'failed to create post' }, status: :not_acceptable
+		end
 	end
 end
